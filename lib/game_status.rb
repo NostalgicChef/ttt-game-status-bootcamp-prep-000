179464958901bf05c3369e3813628a58f37aeba3
Belit Wilson <belitwilson@gmail.com>
2019-09-08 16:14:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,11 +8,11 @@ board = ["X", "X", "X", " ", " ", " ", " ", " ", " ",]
 
 WIN_COMBINATIONS = [
 [0,1,2], # Top row
-middle_row_win = [3,4,5],
-bottom_row_win = [6,7,8],
-left_column_win = [0,3,6],
-middle_column_win = [1,4,7],
-right_column_win = [2,5,8],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
 right_diagonal_win = [2,4,6],
 left_diagonal_win =  [0,4,8],
   ]
